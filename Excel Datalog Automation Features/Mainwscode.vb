b51848cce465a5bb9600d44b9939a857b89854b4
TonyFhh <32705631+TonyFhh@users.noreply.github.com>
2017-10-15 12:15:42 +0800

    Update Mainwscode.vb
    Changes to support multiple changed entries

--- Excel Datalog Automation Features/Mainwscode.vb
+++ Excel Datalog Automation Features/Mainwscode.vb
@@ -1,17 +1,26 @@
 Private Sub Worksheet_Change(ByVal Target As Range)
+'The following code only runs if values of cells from column B changes
     If Not Intersect(Target, Target.Worksheet.Range("B3:B" & Rows.Count)) Is Nothing Then
     Dim checkdup As Integer
-    Debug.Print Target.Value
-    checkdup = WorksheetFunction.CountIf(Range("B:B"), Target.Value)
-    Debug.Print checkdup
+    Dim cel As Range
+    Dim sRow As Long
+
+' Work compaitability with multiple entries, however countif checks against entire range that was changed, chronological
+' appearances of entries are not correctly labelled for frequency.
+For Each cel In Target 'for each row in range of changed cells...
+    sRow = cel.Row
+    checkdup = WorksheetFunction.CountIf(Range("B:B"), Range("B" & sRow).Value) 'Check frequency of changed row
+    ' if frequency > x, change cell colour and font colour to highlight
     If checkdup = 2 Then
-    Target.Interior.Color = RGB(189, 214, 238)
-    Target.Font.ColorIndex = 1
+    Range("B" & sRow).Interior.Color = RGB(189, 214, 238)
+    Range("B" & sRow).Font.ColorIndex = 1
     ElseIf checkdup >= 3 Then
-    Target.Interior.Color = RGB(192, 0, 0)
-    Target.Font.ColorIndex = 2
+    Range("B" & sRow).Interior.Color = RGB(192, 0, 0)
+    Range("B" & sRow).Font.ColorIndex = 2
     End If
-    Call Update
+    Next cel
+    Call Update 'update frequency table
+
 
     End If
 End Sub
